75606f2420911a63058a4bc401bf475772162756
Travis Farmer <travis.farmer@outlook.com>
2021-10-06 14:15:43 -0400

    initial rough idea

---
+++ UDPSendReceiveStringBCslave.ino
@@ -0,0 +1,72 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x02
+};
+IPAddress ip(192, 168, 1, 123);
+
+unsigned int localPort = 8888;       // local port to listen for UDP packets
+
+byte packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+// send an NTP request to the time server at the given address
+void sendBCpacket(const char * address, char * sendBuffer, int PacketSize) {
+  Udp.beginPacket(IPAddress(192, 168, 1, 255), 9999);
+  Udp.write(sendBuffer, PacketSize);
+  Udp.endPacket();
+}
+
+void setup() {
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  //Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // start Ethernet and UDP
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // no point in carrying on, so do nothing forevermore:
+    while (true) {
+      delay(1);
+    }
+  }
+  Udp.begin(localPort);
+}
+
+void loop() {
+  sendBCpacket(timeServer,"buffer", 6); // send an BC packet to the master
+
+  // wait to see if a reply is available
+  delay(1000);
+  if (Udp.parsePacket()) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); // read the packet into the buffer
+
+    Serial.println(packetBuffer);
+  }
+  // wait ten seconds before sending again
+  delay(10000);
+  Ethernet.maintain();
+}
